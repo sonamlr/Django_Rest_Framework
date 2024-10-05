7c360e7b70d208c776fc52ad448380eeeb39ab29
Sonam <sonamlodhi6251@gmail.com>
2024-10-05 11:17:15 +0530

    Class Based API View and Generic API View

--- drf2/api/urls.py
+++ drf2/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    # function based view url 
     path('studentapi/', views.student_api),
     path('studentapi/<int:pk>', views.student_api),
+
+    # class  based view url
+    path('cstudentapi/', views.StudentAPI.as_view()),
+    path('cstudentapi/<int:pk>', views.StudentAPI.as_view()),
+
+    #generic view api url
+    path('gstudentapi/', views.GPStudentAPI.as_view()),
+    path('gstudentapi/<int:pk>', views.PDStudentAPI.as_view()),
 ]
--- drf2/api/views.py
+++ drf2/api/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
+# for function based apiview 
+from rest_framework.decorators import api_view
+# for class based apiview
+from rest_framework.views import APIView
+
+# for generic apiview 
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin
 # Create your views here.
 
+# function based apiview 
 @api_view(['GET','POST', 'PUT', 'PATCH', 'DELETE'])
 def student_api(request, pk=None):
     if request.method == 'GET':
@@ -46,4 +54,66 @@ def student_api(request, pk=None):
         stu.delete()
         return Response({'msg': 'Data Deleted'})
     
+# class based apiview 
+class StudentAPI(APIView):
+    def get(self, request,pk=None, format=None):
+        if pk is not None:
+            stu = Student.objects.get(pk=pk)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
     
+    def post(self, request, format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,  status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk, format=None):
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Updated'})
+        return Response(serializer.errors)
+    
+    def patch(self, request, pk, format=None):
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Updated'})
+        return Response(serializer.errors)
+    
+    def delete(self, request, pk, format=None):
+        stu = Student.objects.get(pk=pk)
+        stu.delete()
+        return Response({'msg': 'Data Deleted'})
+ 
+    
+# generic api view 
+class GPStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class PDStudentAPI(GenericAPIView,UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin ):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
+
+
+
+
